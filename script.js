6be6da1f6253d563323c1b85381368bc21e59456
lincolas <84355259+Lincolas@users.noreply.github.com>
2021-11-29 23:46:15 +0000

    Made cipher but needs debugging

--- script.js
+++ script.js
@@ -6,6 +6,49 @@ var zCode = aCode + range; //letter "z" for the computer
 
 //get the HTML elements
 var input = document.getElementById("in");
-var encrypt = doeument.getElementById("encrypt");
+var encrypt = document.getElementById("encrypt");
 var decrypt = document.getElementById("decrypt");
 var output = document.getElementById("out");
+
+encrypt.onclick = encipher;
+decrypt.onclick = decipher;
+
+function encipher() {
+  var message = input.value.toUpperCase();
+  output.innerHTML = "";
+
+  for (var i = 0; i < message.length; i++) {
+    var letter = message.charCodeAt(i);
+    var newLetter = letter;
+
+    if (letter >= aCode && letter <= zCode) {
+      newLetter += key;
+
+      if (newLetter >= zCode) {
+        newLetter -= range;
+      }
+    }
+    newLetter = String.fromCharCode(newLetter);
+    output.innerHTML += newLetter;
+  }
+}
+
+function decipher() {
+  var message = input.value.toUpperCase();
+  output.innerHTML = "";
+
+  for (var i = 0; i < message.length; i++) {
+    var letter = message.charCodeAt(i);
+    var newLetter = letter;
+
+    if (letter >= aCode && letter <= zCode) {
+      newLetter -= key;
+
+      if (newLetter >= zCode) {
+        newLetter += range;
+      }
+    }
+    newLetter = String.fromCharCode(newLetter);
+    output.innerHTML += newLetter;
+  }
+}
