9a8d79a787a6f0df6b6aff5d7a6e7985285ba905
lincolas <84355259+Lincolas@users.noreply.github.com>
2021-11-29 23:26:56 +0000

    Finished the CSS file

---
+++ script.js
@@ -0,0 +1,11 @@
+//variables
+var key = 5; //number being shifted
+var range = 26; //leters in the alphabet
+var aCode = 65; //letter "a" is for the computer
+var zCode = aCode + range; //letter "z" for the computer
+
+//get the HTML elements
+var input = document.getElementById("in");
+var encrypt = doeument.getElementById("encrypt");
+var decrypt = document.getElementById("decrypt");
+var output = document.getElementById("out");
